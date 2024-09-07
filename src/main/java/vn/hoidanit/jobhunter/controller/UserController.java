1cf1bf0180421153e94777f045d840bdd3cf1e8d
hoangchuong <lehoangchuong.lk@gmail.com>
2024-09-07 16:00:07 +0700

    api /logiin

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -3,19 +3,22 @@ package vn.hoidanit.jobhunter.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.util.error.IdInvalidException;
 
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    public final UserService userService;
-
-    public UserController(UserService userService) {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
@@ -39,13 +42,18 @@ public class UserController {
 
     @PostMapping("/users/create")
     public ResponseEntity<User> createNewUser(@RequestBody User user) {
+        user.setPassword(this.passwordEncoder.encode(user.getPassword()));
         User user1 = this.userService.handleCreateUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) throws IdInvalidException {
+        if(id > 1000){
+            throw new IdInvalidException("id lon hong 1000");
+        }
         this.userService.deleteUserById(id);
         return ResponseEntity.status(HttpStatus.OK).body("User deleted.");
     }
+
 }
---
+++ src/main/java/vn/hoidanit/jobhunter/util/FormatRestResponse.java
@@ -0,0 +1,43 @@
+package vn.hoidanit.jobhunter.util;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import vn.hoidanit.jobhunter.domain.RestResponse;
+
+@ControllerAdvice
+public class FormatRestResponse implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+        HttpServletResponse servletRequest = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletRequest.getStatus();
+
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(status);
+        if(status >= 400) {
+          return body;
+        }else {
+            res.setData(body);
+            res.setMessage("Call api successfully");
+
+        }
+
+        return res;
+    }
+
+}
