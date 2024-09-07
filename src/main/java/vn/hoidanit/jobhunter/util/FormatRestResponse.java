b8688bd04e45ea7b1447c6d83567ecbd929e67cd
hoangchuong <lehoangchuong.lk@gmail.com>
2024-09-07 21:35:52 +0700

    done spring securiry

--- src/main/java/vn/hoidanit/jobhunter/util/FormatRestResponse.java
+++ src/main/java/vn/hoidanit/jobhunter/util/FormatRestResponse.java
@@ -29,9 +29,13 @@ public class FormatRestResponse implements ResponseBodyAdvice<Object> {
 
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(status);
-        if(status >= 400) {
-          return body;
-        }else {
+
+        if(body instanceof String){
+            return body;
+        }
+        if (status >= 400) {
+            return body;
+        } else {
             res.setData(body);
             res.setMessage("Call api successfully");
 
